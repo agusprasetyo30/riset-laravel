8077cbaaedbb13de1a0d651715261b2f4dc13676
Agus Prasetyo <agusprasetyo1889@gmail.com>
2020-07-30 14:01:32 +0700

    finish API Restfull laravel

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('todos', 'TodoApiController@index');
+    Route::get('todos/{todo}', 'TodoApiController@show');
+    Route::post('todos', 'TodoApiController@store');
+    Route::put('todos/{todo}', 'TodoApiController@update');
+    Route::delete('todos/{todo}', 'TodoApiController@delete');
 });
 
-Route::get('todos', 'TodoApiController@index');
-Route::get('todos/{todo}', 'TodoApiController@show');
-Route::post('todos', 'TodoApiController@store');
-Route::put('todos/{todo}', 'TodoApiController@update');
-Route::delete('todos/{todo}', 'TodoApiController@delete');
+
 
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
