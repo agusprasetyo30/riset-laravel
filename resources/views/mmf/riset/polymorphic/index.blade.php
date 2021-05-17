6e65e866326ac209550f054d87bc0b574e2458c7
Agus Prasetyo <agusprasetyo1889@gmail.com>
2021-05-17 14:09:21 +0700

    menambahkan model,  Controller tag & menambahkan tabel taggable

--- resources/views/mmf/riset/polymorphic/index.blade.php
+++ resources/views/mmf/riset/polymorphic/index.blade.php
@@ -32,26 +32,78 @@
       <h1>Polymorphisme</h1>
       <small>Menerapkan metode <b>Polymorphisme</b> dalam mengembangkan sistem, selain menggunakan metode yang telah ada seperti eloquent relationship dll</small>
       <br>
-      <small>Contoh dalam kasus ini adalah <b>Post</b> dan <b>Video</b> Memiliki <b>Comment</b></small>
+      <small>-  <b>Post</b> dan <b>Video</b> Memiliki <b>Comment</b></small> (One to Many)
+      <br>
+      <small>- <b>Post</b>, <b>Video</b>, <b>Tags</b> yang dimana post dan Video memiliki tags dan tags bisa memiliki beberapa post dan video</small> (Many to many)
+
    </div>
    
-   <div class="row justify-content-center mt-5">
-      <div class="col-md-3">
-         <div class="card">
-            <a href="{{ route('test.poly.video.index') }}" class="text-decoration-none">
-               <div class="card-body text-center button-background">
-                  <div class="p-3 font-weight-bolder font-20">Videos</div>
+   <div class="row">
+      <div class="col-md-6">
+         <div class="card mt-5">
+            <div class="card-body">
+               <div class="row justify-content-center">
+                  <div class="col-md-12 text-center mt-2 mb-2">
+                     <h4>One to Many</h4>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="card">
+                        <a href="{{ route('test.poly.video.index') }}" class="text-decoration-none">
+                           <div class="card-body text-center button-background">
+                              <div class="p-3 font-weight-bolder font-20">Videos</div>
+                           </div>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="card">
+                        <a href="{{ route('test.poly.post.index') }}" class="text-decoration-none">
+                           <div class="card-body text-center button-background">
+                              <div class="p-3 font-weight-bolder font-20">Posts</div>
+                           </div>
+                        </a>
+                     </div>
+                  </div>
                </div>
-            </a>
+            </div>
          </div>
       </div>
-      <div class="col-md-3">
-         <div class="card">
-            <a href="{{ route('test.poly.post.index') }}" class="text-decoration-none">
-               <div class="card-body text-center button-background">
-                  <div class="p-3 font-weight-bolder font-20">Posts</div>
+      <div class="col-md-6">
+         <div class="card mt-5">
+            <div class="card-body">
+               <div class="row justify-content-center">
+                  <div class="col-md-12 text-center mt-2 mb-2">
+                     <h4>Many to Many</h4>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="card">
+                        <a href="#" class="text-decoration-none">
+                           <div class="card-body text-center button-background">
+                              <div class="p-3 font-weight-bolder font-20">Videos</div>
+                           </div>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="card">
+                        <a href="#" class="text-decoration-none">
+                           <div class="card-body text-center button-background">
+                              <div class="p-3 font-weight-bolder font-20">Posts</div>
+                           </div>
+                        </a>
+                     </div>
+                  </div>
+                  <div class="col-md-6">
+                     <div class="card">
+                        <a href="#" class="text-decoration-none">
+                           <div class="card-body text-center button-background">
+                              <div class="p-3 font-weight-bolder font-20">Tags</div>
+                           </div>
+                        </a>
+                     </div>
+                  </div>
                </div>
-            </a>
+            </div>
          </div>
       </div>
    </div>
