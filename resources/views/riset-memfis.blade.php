5c17951756ca03392140316347e345a1b8538a0c
Agus Prasetyo <agusprasetyo1889@gmail.com>
2021-06-17 16:48:14 +0700

    menambahkan tombol logout

--- resources/views/riset-memfis.blade.php
+++ resources/views/riset-memfis.blade.php
@@ -52,12 +52,19 @@
          <div class="nav navbar-nav ml-auto">
                @if (Route::has('login'))
                   @auth
+                     <a class="nav-item nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                     document.getElementById('logout-form').submit();">Logout</a>
+
+                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                     </form>
+
                      <a class="nav-item nav-link" href="{{ url('/home') }}">Home</a>
                   @else
                      <a class="nav-item nav-link" href="{{ route('login') }}">Login</a>
 
                      @if (Route::has('register'))
-                           <a class="nav-item nav-link" href="{{ route('register') }}">Register</a>
+                        <a class="nav-item nav-link" href="{{ route('register') }}">Register</a>
                      @endif
                   @endauth
                @endif
