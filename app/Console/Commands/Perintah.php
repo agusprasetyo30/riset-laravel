1346fcd8eae9da0da880eb4ba6530e779264b473
Agus Prasetyo <35440229+agusprasetyo1@users.noreply.github.com>
2021-09-09 13:15:23 +0700

    Finish command console riset

--- app/Console/Commands/Perintah.php
+++ app/Console/Commands/Perintah.php
@@ -2,25 +2,26 @@
 
 namespace App\Console\Commands;
 
-use Composer\Composer;
 use Illuminate\Console\Command;
+use Illuminate\Support\Composer;
 
 class Perintah extends Command
 {
     protected $composer;
+    protected $nama;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
     protected $signature = 'riset:perintah
-                            {nama : nama/jenis perintah sesuai dengan request, eg: composer-dump, cache-clear, route-clear}
+                            {start : Jalankan perintah , eg: start composer-dump, start cache-clear, start route-clear}
                             {--a|all=true : Mengeksekusi semua perintah}
                             {--c|clear-cache : Mengeksekusi perintah clear cache}
                             {--r|clear-route : Mengeksekusi perintah clear cache route}
-                            {--d|dump-autoload : Mengeksekusi perintah composer dump-autoload}
-                            {--u|composer-update : Mengeksekusi perintah composer update}
-                            {--i|composer-install : Mengeksekusi perintah composer install}';
+                            {--l|clear-all : Mengeksekusi perintah semua clear}
+                            {--d|dump-autoload : Mengeksekusi perintah composer dump-autoload}';
 
     /**
      * The console command description.
@@ -47,9 +48,20 @@ class Perintah extends Command
      */
     public function handle()
     {
-        $this->input->setOption('clear-cache', true);
+        // ;
+        // $this->line($this->option('clear-cache'));
+        
+        if ($this->option('all')) {
+            $this->input->setOption('clear-cache', true);
 
-        // $this->makeClearCache();
+            $this->input->setOption('dump-autoload', true);
+        }
+
+        $this->start    = $this->argument('start');
+
+        $this->makeClearCache();
+        $this->makeClearRoute();
+        $this->makeDumpAutoload();
 
         // return 0;
     }
@@ -57,8 +69,49 @@ class Perintah extends Command
     protected function makeClearCache()
     {
         if ($this->option('clear-cache')) {
-            $this->line("Saya memilih clear cache");
+            $this->info("[START] Clear the Application Cache..........");
 
+            $this->call('cache:clear');
+            $this->call('config:clear');
+
+            $this->info('[DONE] Clear the Application Cache..........');
+            $this->line('');
+        }
+    }
+
+    protected function makeClearRoute()
+    {
+        if ($this->option('clear-route')) {
+            $this->info("[START] Clear the Rout & View Cache..........");
+            
+            $this->call('route:clear');
+            $this->call('view:clear');
+
+            $this->info("[DONE] Clear the Route & View Cache..........");
+        }
+    }
+
+    protected function makeDumpAutoload()
+    {
+        if ($this->option('dump-autoload')) {
+            $this->info("[START] Dump Autoload ..........");
+
+            $this->composer->dumpAutoloads();
+
+            $this->info("[DONE] Dump Autoload ..........");
+        }
+    }
+
+    protected function makeClearCacheAll()
+    {
+        if ($this->option('clear-all')) {
+            $this->info("[START] Clear Cache All ..........");
+
+            $this->input->setOption('clear-cache', 'clear-cache');
+
+            $this->input->setOption('clear-route', 'clear-route');
+
+            $this->info("[DONE] Clear Cache All ..........");
         }
     }
 }
